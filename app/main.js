f89f833f5ae13990a2154a8df1d832bd8671b44e
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-02-17 17:57:31 +0900

    Remove: front 관련 코드 삭제

--- app/main.js
+++ app/main.js
@@ -3,10 +3,6 @@ const cart = require("./src/routes/routes.js");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.set("views", "./src/views");
-app.set("view engine", "ejs");
-app.use(express.static(`${__dirname}/src/public`));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
